b3ec38d960c7faf0c9eecd03a30e8d36cb1fd590
Artem <artem.krivtsov.96@mail.ru>
2023-06-20 19:31:56 +0300

    Homewwork2 finish

---
+++ Homework_20.06.23/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 2: Напишите программу, которая выводит третью цифру заданного
+//числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите трёхзначное число ");
+int numb = int.Parse(Console.ReadLine());
+int thirdNumb = numb%10;
+
+
+
+if (numb < 100)
+{
+  Console.WriteLine($"Напишите третью цифру");
+}
+else
+{
+Console.WriteLine($"Третья цифра в заданном числе равна {thirdNumb} ");
+}
---
+++ Homework_20.06.23/task3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 3: Напишите программу, которая принимает на вход цифру, обозначащую день
+//недели, и проверяет, является ли этот дени выходным.
+
+Console.Write("Введите число ");
+int day = int.Parse(Console.ReadLine());
+
+switch (day)
+{
+  case (1):
+    Console.Write("Рабочий день");
+    break;
+  case (2):
+    Console.Write("Рабочий день");
+    break;
+  case (3):
+    Console.Write("Рабочий день");
+    break;
+  case (4):
+    Console.Write("Рабочий день");
+    break;
+  case (5):
+    Console.Write("Рабочий день");
+    break;
+  case (6):
+    Console.Write("Выходной день");
+    break;
+  case (7):
+    Console.Write("Выходной день");
+    break;
+  
+  default:
+  Console.Write("Введите число от 1 до 7");
+  break;
+}
