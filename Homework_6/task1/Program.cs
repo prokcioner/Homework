73b5b28990fd711471b6213461bdebb35436771b
Artem <artem.krivtsov.96@mail.ru>
2023-07-10 17:15:50 +0300

    Homework_6 comp

---
+++ Homework_6/task1/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+int Promt(string message)
+{
+  Console.Write(message);
+  int result = int.Parse(Console.ReadLine());
+  return result;
+}
+void printArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+}
+int positiveNum(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > 0)
+    {
+      count++;
+    }
+  }
+return count;
+}
+
+
+int length = Promt("Напишите сколько чисел вы хотите ввести: ");
+int[] array = new int[length];
+int num = 0;
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = Promt($"Введите {i + 1} число: ");
+}
+Console.WriteLine($"Пользователь ввёл {positiveNum(array)} чисел больше нуля");
+
+
+
+
+
+
+
+
+printArray(array); 
+
+
---
+++ Homework_6/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+double Promt(string massage)
+{
+  Console.Write(massage);
+  double n = double.Parse(Console.ReadLine());
+  return n;
+}
+
+
+double b1 = Promt("Введите b1: ");
+double k1 = Promt("Введите k1: ");
+double b2 = Promt("Введите b2: ");
+double k2 = Promt("Введите k2: ");
+
+double x = (b2-b1)/(k1-k2);
+double y = k1*x + b1;
+
+Console.WriteLine($"Точка пересечения прямых: ({x}; {y})");
+
