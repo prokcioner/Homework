8f1b57a712344d20a5c7d35641589cfe42cd1cb8
Artem <artem.krivtsov.96@mail.ru>
2023-07-29 17:36:55 +0300

    1&2 task in H9

---
+++ Homework_9/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+
+
+
+void Numb(int N)
+{
+  
+  if (N == 0){System.Console.WriteLine(" ");  }
+  else
+  {
+    System.Console.Write($"{N} ");
+    Numb(N-1);
+    
+  }
+
+}
+
+System.Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Numb(n);
---
+++ Homework_9/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int sum(int a, int b)
+{
+if (b < a){return 0;}
+else
+{
+  return a + sum(a + 1, b);
+
+}
+
+}
+
+System.Console.Write("Введите первое число: ");
+int firstN = int.Parse(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int secondN = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Ответ: {sum(firstN, secondN)}");
